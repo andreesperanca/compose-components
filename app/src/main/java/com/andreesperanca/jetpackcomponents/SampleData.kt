d290efd6783fd48b3e3d705d5b99aca1b12c7879
André Luiz <andreluizesperancacorreia@gmail.com>
2023-05-27 09:45:59 -0300

    Work in explorer screen

---
+++ app/src/main/java/com/andreesperanca/jetpackcomponents/SampleData.kt
@@ -0,0 +1,18 @@
+package com.andreesperanca.jetpackcomponents
+
+import androidx.annotation.DrawableRes
+
+data class HighlightGroup(
+    @DrawableRes val image: Int,
+    val name: String
+)
+
+val highLightGroups = listOf(
+    HighlightGroup(image = R.drawable.ic_launcher_foreground, "Xoxo"),
+    HighlightGroup(image = R.drawable.ic_launcher_foreground, "Xoxo"),
+    HighlightGroup(image = R.drawable.ic_launcher_foreground, "Xoxo"),
+    HighlightGroup(image = R.drawable.ic_launcher_foreground, "Xoxo")
+)
+
+
+
